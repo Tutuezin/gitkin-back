64eefb9252b04081cc994d47bf17b6c341fcf20c
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-10-03 17:21:45 -0300

    feat: create insertUser on authRepository

--- src/repositories/authRepository.ts
+++ src/repositories/authRepository.ts
@@ -1,7 +1,12 @@
 import { prisma } from "../config/database";
+import * as authTypes from "../types/authTypes";
 
 export async function findEmail(email: string) {
   const result = await prisma.user.findUnique({ where: { email } });
 
   return result;
+}
+
+export async function createUser(user: authTypes.IUserData) {
+  return await prisma.user.create({ data: user });
 }
