705b410fc16d47bbb812d42c93d2f12bf0682105
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-10-04 17:02:02 -0300

    feat: create insertRepository service

--- src/services/repositoryService.ts
+++ src/services/repositoryService.ts
@@ -1,5 +1,13 @@
 import * as repositoryRepository from "../repositories/repositoryRepository";
+import * as repositoryTypes from "../types/repositoryTypes";
 
-export async function getRepositories(user: string, userId: number) {
-  return await repositoryRepository.getRepositories(user, userId);
+export async function getRepositories(userId: number) {
+  return await repositoryRepository.getRepositories(userId);
+}
+
+export async function insertRepository(
+  userId: number,
+  repositoryData: repositoryTypes.IRepositoryData
+) {
+  return await repositoryRepository.insertRepository(userId, repositoryData);
 }
