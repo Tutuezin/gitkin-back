b814c843426ff033cdcfb172316be2642e0b961b
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-10-04 15:38:20 -0300

    feat: create updateInfos router

--- src/routes/profileRouter.ts
+++ src/routes/profileRouter.ts
@@ -2,9 +2,16 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/validations/schemaValidator";
 import * as profileController from "../controllers/profileController";
 import { validateToken } from "../middlewares/validations/tokenValidator";
+import { updateInfosSchema } from "../schemas/profileSchema";
 
 const profileRouter = Router();
 
 profileRouter.get("/:username", profileController.getProfileInfos);
+profileRouter.put(
+  "/:username",
+  validateToken,
+  validateSchema(updateInfosSchema),
+  profileController.updateInfos
+);
 
 export default profileRouter;
