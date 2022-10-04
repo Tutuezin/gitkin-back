bdbc6ddf7d7856b4dbd3d7a754529102f9f1b225
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-10-04 17:01:46 -0300

    feat: create insertRepository route

--- src/routes/repositoryRouter.ts
+++ src/routes/repositoryRouter.ts
@@ -1,12 +1,21 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validations/schemaValidator";
 import * as repositoryController from "../controllers/repositoryController";
+import { repositoryInfosSchema } from "../schemas/repositorySchema";
+import { validateToken } from "../middlewares/validations/tokenValidator";
 
 const repositoryRouter = Router();
 
 repositoryRouter.get(
   "/:username/repository/:userId",
   repositoryController.getRepositories
+);
+
+repositoryRouter.post(
+  "/:username/repository/:userId",
+  validateToken,
+  validateSchema(repositoryInfosSchema),
+  repositoryController.insertRepository
 );
 
 export default repositoryRouter;
