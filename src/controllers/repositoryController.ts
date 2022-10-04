1ed67d49066e4e7369b0923795e8209e36f575fd
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-10-04 17:01:52 -0300

    feat: create insertRepository controller

--- src/controllers/repositoryController.ts
+++ src/controllers/repositoryController.ts
@@ -4,10 +4,19 @@ import * as repositoryService from "../services/repositoryService";
 export async function getRepositories(req: Request, res: Response) {
   const { username, userId } = req.params;
 
-  const repositories = await repositoryService.getRepositories(
-    username,
-    Number(userId)
-  );
+  const repositories = await repositoryService.getRepositories(Number(userId));
 
   res.status(200).send(repositories);
+}
+
+export async function insertRepository(req: Request, res: Response) {
+  const { username, userId } = req.params;
+  const repositoryData = req.body;
+
+  const repository = await repositoryService.insertRepository(
+    Number(userId),
+    repositoryData
+  );
+
+  res.status(201).send(repository);
 }
