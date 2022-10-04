756642fe387f808cffd5b8dc795f2941c8959049
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-10-04 15:38:46 -0300

    feat: create updateInfos service

--- src/services/profileService.ts
+++ src/services/profileService.ts
@@ -3,3 +3,7 @@ import * as profileRepository from "../repositories/profileRepository";
 export async function getProfileInfos(user: string) {
   return await profileRepository.getProfileInfos(user);
 }
+
+export async function updateInfos(user: string, infos: object) {
+  return await profileRepository.updateInfos(user, infos);
+}
