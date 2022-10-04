d221ecf99cd3af334527efe58f9888316af4300c
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-10-04 17:02:17 -0300

    feat: create insertRepository on repository

--- src/repositories/repositoryRepository.ts
+++ src/repositories/repositoryRepository.ts
@@ -1,8 +1,19 @@
 import { prisma } from "../config/database";
+import * as repositoryTypes from "../types/repositoryTypes";
 
-export async function getRepositories(user: string, userId: number) {
+export async function getRepositories(userId: number) {
   const result = await prisma.repository.findMany({
     where: { userId },
+  });
+  return result;
+}
+
+export async function insertRepository(
+  userId: number,
+  repositoryData: repositoryTypes.IRepositoryData
+) {
+  const result = await prisma.repository.create({
+    data: { userId, ...repositoryData },
   });
   return result;
 }
