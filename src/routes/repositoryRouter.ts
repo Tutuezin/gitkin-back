f7c6bcd3d1e39c1753041f0e3438b24d68be80ab
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-10-04 17:44:49 -0300

    feat: create deleteRepository route

--- src/routes/repositoryRouter.ts
+++ src/routes/repositoryRouter.ts
@@ -6,16 +6,26 @@ import { validateToken } from "../middlewares/validations/tokenValidator";
 
 const repositoryRouter = Router();
 
+//TODO verificar quando o usarname n eh passado ou n existe
 repositoryRouter.get(
   "/:username/repository/:userId",
   repositoryController.getRepositories
 );
 
+//TODO verificar quando o usarname n eh passado ou n existe
 repositoryRouter.post(
   "/:username/repository/:userId",
   validateToken,
   validateSchema(repositoryInfosSchema),
   repositoryController.insertRepository
+);
+
+//TODO verificar quando o usarname n eh passado ou n existe
+//TODO deixar essa rota autenticada
+repositoryRouter.delete(
+  "/:username/repository/:userId",
+  //validateToken,
+  repositoryController.deleteRepository
 );
 
 export default repositoryRouter;
