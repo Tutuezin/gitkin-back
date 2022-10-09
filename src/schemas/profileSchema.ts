4ea7fc9824663587c359df27bc7cf348a3113311
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-10-09 00:39:26 -0300

    feat: create update socials schema

--- src/schemas/profileSchema.ts
+++ src/schemas/profileSchema.ts
@@ -1,10 +1,23 @@
+import { empty } from "@prisma/client/runtime";
 import Joi from "joi";
 
 export const updateInfosSchema = Joi.object({
+  name: Joi.string().max(25).optional(),
   picture: Joi.string()
+    .allow("")
     .allow(null)
     .regex(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/)
     .optional(),
-  occupation: Joi.string().allow(null).max(30).optional(),
-  aboutMe: Joi.string().allow(null).optional(),
+  occupation: Joi.string().allow("").allow(null).max(30).optional(),
+  aboutMe: Joi.string().allow("").allow(null).optional(),
+});
+
+export const updateSocialsSchema = Joi.object({
+  location: Joi.string().allow("").allow(null).optional(),
+  work: Joi.string().allow("").allow(null).optional(),
+  github: Joi.string().allow("").allow(null).optional(),
+  linkedin: Joi.string().allow("").allow(null).optional(),
+  twitter: Joi.string().allow("").allow(null).optional(),
+  website: Joi.string().allow("").allow(null).optional(),
+  email: Joi.string().allow("").allow(null).optional(),
 });
