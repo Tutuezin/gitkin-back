d1f55e2bdfca50c20e04e5308ecd251908bbc4ba
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-10-09 00:38:57 -0300

    feat: create update socials router

--- src/routes/profileRouter.ts
+++ src/routes/profileRouter.ts
@@ -2,19 +2,27 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/validations/schemaValidator";
 import * as profileController from "../controllers/profileController";
 import { validateToken } from "../middlewares/validations/tokenValidator";
-import { updateInfosSchema } from "../schemas/profileSchema";
+import {
+  updateInfosSchema,
+  updateSocialsSchema,
+} from "../schemas/profileSchema";
 
 const profileRouter = Router();
 
-//TODO verificar quando o usarname n eh passado ou n existe
 profileRouter.get("/:username", profileController.getProfileInfos);
 
-//TODO verificar quando o usarname n eh passado ou n existe
 profileRouter.put(
   "/:username",
   validateToken,
   validateSchema(updateInfosSchema),
   profileController.updateInfos
+);
+
+profileRouter.put(
+  "/:username/:userId",
+  validateToken,
+  validateSchema(updateSocialsSchema),
+  profileController.updateSocials
 );
 
 export default profileRouter;
