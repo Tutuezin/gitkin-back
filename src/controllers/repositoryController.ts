862d0c45b61950080be8dedb748bdd28e0c2e281
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-11-22 14:04:04 -0300

    feat: edit repository controller

--- src/controllers/repositoryController.ts
+++ src/controllers/repositoryController.ts
@@ -22,6 +22,20 @@ export async function insertRepository(req: Request, res: Response) {
   res.status(201).send(repository);
 }
 
+export async function editRepository(req: Request, res: Response) {
+  const { username, userId, repoId } = req.params;
+  const repositoryInfo = req.body;
+
+  const repository = await repositoryService.editRepository(
+    Number(userId),
+    username,
+    Number(repoId),
+    repositoryInfo
+  );
+
+  res.status(200).send(repository);
+}
+
 export async function deleteRepository(req: Request, res: Response) {
   const { username, userId, repoId } = req.params;
 
