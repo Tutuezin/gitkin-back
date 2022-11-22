925d00c8883e268cef2dc12ca1f1f0c24758a57b
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-11-22 14:04:17 -0300

    feat: edit repository service

--- src/services/repositoryService.ts
+++ src/services/repositoryService.ts
@@ -20,6 +20,28 @@ export async function insertRepository(
   return await repositoryRepository.insertRepository(userId, repositoryData);
 }
 
+export async function editRepository(
+  userId: number,
+  username: string,
+  repositoryId: number,
+  repositoryInfo: repositoryTypes.IRepositoryData
+) {
+  const userNameExists = await repositoryRepository.findUserName(username);
+  const userIdExists = await repositoryRepository.findUserId(userId);
+  const repositoryIdExists = await repositoryRepository.findRepositoryId(
+    repositoryId
+  );
+
+  repositoryUtils.verifyUserNameExists(userNameExists);
+  repositoryUtils.verifyUserIdExists(userIdExists);
+  repositoryUtils.verifyRepositoryIdExists(repositoryIdExists);
+
+  return await repositoryRepository.editRepository(
+    repositoryId,
+    repositoryInfo
+  );
+}
+
 export async function deleteRepository(
   userId: number,
   username: string,
